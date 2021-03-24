1a8d21d370da277e93634a0763ed98d27362ae27
fakhrulkhusaeni <fakhrulkhusaeni@gmail.com>
2021-03-24 14:06:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pc_kota_tegal/ProduckActivity.java
@@ -0,0 +1,14 @@
+package com.example.pc_kota_tegal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProduckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produck);
+    }
+}
